642da8266af2cf602fda38fb9f6a846520f68fc5
unknown <key.developer05@gmail.com>
2021-01-22 00:09:31 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 
-
-// class Component
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onClick={()=> {console.log("I am clicked.")}}/>
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 // functional Component
 const App = () => {
-  return (<div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-  </div>);
+
+  const profiles = [
+    {name: "Taro", age: 10 },
+    {name: "Hanako", age: 5 },
+    {name: "Noname" }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          // index.js:1 Warning: Each child in a list should have a unique "key" prop.
+          // ReactがDOMを作成する際に変更点のみを反映させるため、
+          // DOM管理にはキーが必要なのでキーがない場合上記エラーが出る
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
+  );
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old !</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
